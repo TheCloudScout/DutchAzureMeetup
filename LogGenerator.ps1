a48bea7c352fe38571ce48040480adacf34da9c4
Koos Goossens <koos.goossens@wortell.nl>
2023-10-19 21:42:44 +0200

    Updates

--- LogGenerator.ps1
+++ LogGenerator.ps1
@@ -7,7 +7,7 @@ param (
 )
 
 $appId          = "5313871f-8a8f-45a7-be85-bf9bc395e141"                            # the tenant ID in which the Data Collection Endpoint resides
-$appSecret      = "4aw8Q~LBqzeiaBERBURqdjHhADFtKB5jlhFqRbh7"                        # the tenant ID in which the Data Collection Endpoint resides
+$appSecret      = ""                                                                # the tenant ID in which the Data Collection Endpoint resides
 $tenantId       = "8c884f39-eb63-49ee-83e4-7ebe060b8e5a"                            # the secret created for the above app - never store your secrets in the source code
 $dcrImmutableId = "dcr-cb6c175e376548059f7d9e6b55230ee4"                            # immutable ID of the Data Collection Rule
 $dceUri         = "https://bitdefend-o877.westeurope-1.ingest.monitor.azure.com"    # the URI of the Data Collection Endpoint
@@ -22,7 +22,7 @@ $payload = @{
 }
 
 # Obtain a bearer token used to authenticate against the data collection endpoint
-$scope          = [System.Web.HttpUtility]::UrlEncode("https://monitor.azure.com//.default")   
+$scope          = "https%3a%2f%2fmonitor.azure.com%2f%2f.default"
 $body           = "client_id=$appId&scope=$scope&client_secret=$appSecret&grant_type=client_credentials";
 $headers        = @{"Content-Type" = "application/x-www-form-urlencoded" };
 $uri            = "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token"
